a8886b67fa33b962c7e30f23cb8133cac7c197e6
Nicole Candiotti <nccandiotti@gmail.com>
2022-02-23 16:38:16 -0500

    commit tree ab2acda322bc34852cf900e84ffeff8083f1ce62

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
 
+class Book
+    attr_accessor :author, :genre, :title, :page_count
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
